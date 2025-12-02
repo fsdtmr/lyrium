a489136431fc17232a6d186e50c98a877db7333d
fsdtmr <166978900+fsdtmr@users.noreply.github.com>
2025-12-02 19:59:18 +0000

    remove db test

--- test/db_test.dart
+++ test/db_test.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:lyrium/datahelper.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  final dbh = DataHelper.instance;
-  for (var i = 0; i < 2; i++) {
-    final f = await dbh.insertDraft("title$i", "artist$i", "lyrics$i");
-    assert(f.isNotEmpty);
-  }
+  // WidgetsFlutterBinding.ensureInitialized();
+  // final dbh = DataHelper.instance;
+  // for (var i = 0; i < 2; i++) {
+  //   final f = await dbh.insertDraft("title$i", "artist$i", "lyrics$i");
+  //   assert(f.isNotEmpty);
+  // }
 }
