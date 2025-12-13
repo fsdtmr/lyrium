a9e1c9d5c0e7262ce1388d9949dd2e4955bcfba6
fsdtmr <166978900+fsdtmr@users.noreply.github.com>
2025-12-13 14:35:04 +0000

    refactor(view): :art: LoaderWidget

---
+++ lib/widgets/loader.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class LoaderWidget<T> extends StatelessWidget {
+  final Future<T> future;
+  final Route Function(T? result) onRoute;
+  final Widget Function(BuildContext context, ConnectionState connectionState)
+  onConnection;
+  final Widget Function(BuildContext context, Object? error) onError;
+
+  const LoaderWidget({
+    super.key,
+    required this.future,
+    required this.onRoute,
+    required this.onConnection,
+    required this.onError,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Loading...")),
+      body: FutureBuilder<T>(
+        future: future,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState != ConnectionState.done) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.connectionState == ConnectionState.done) {
+            WidgetsBinding.instance.addPostFrameCallback((_) {
+              Navigator.pushReplacement(context, onRoute(snapshot.data));
+            });
+            return Container();
+          } else if (snapshot.hasError) {
+            return onError(context, snapshot.error);
+          } else {
+            return Center(child: Text("Unexpected state"));
+          }
+        },
+      ),
+    );
+  }
+}
