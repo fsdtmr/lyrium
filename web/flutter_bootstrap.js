89cfa1aa474836d5b15e98f9e3191f35b597b1c0
fsdtmr <166978900+fsdtmr@users.noreply.github.com>
2026-01-04 15:47:57 +0000

    feat(offline): :bookmark: offline support for pwa

---
+++ web/flutter_bootstrap.js
@@ -0,0 +1,34 @@
+{ { flutter_js } }
+{ { flutter_build_config } }
+
+_flutter.loader.load({
+    onEntrypointLoaded: async function onEntrypointLoaded(engineInitializer) {
+        let appRunner = await engineInitializer.initializeEngine({
+            hostElement: document.querySelector("#flutterview"),
+        });
+        await appRunner.runApp();
+    },
+});
+
+// self.addEventListener("install", (event) => {
+//     event.waitUntil(
+//         caches.open("flutter-app-cache").then((cache) => {
+//             return cache.addAll([
+//                 "/",
+//                 "/index.html",
+//                 "/main.dart.js",
+//             ]);
+//         })
+//     );
+// });
+
+// self.addEventListener("fetch", (event) => {
+//     event.respondWith(
+//         caches.match(event.request).then((cachedResponse) => {
+//             if (cachedResponse) {
+//                 return cachedResponse;
+//             }
+//             return fetch(event.request);
+//         })
+//     );
+// });
